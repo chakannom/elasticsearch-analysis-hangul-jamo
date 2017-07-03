7863cecb87d0dd2e7a6371a5c4525e07d7018a59
Seon Yu <genteelson@gmail.com>
2017-07-03 16:25:52 +0900

    불필요부분 제거

--- src/main/java/org/elasticsearch/plugin/HangulJamoPlugin.java
+++ src/main/java/org/elasticsearch/plugin/HangulJamoPlugin.java
@@ -1,13 +1,8 @@
 package org.elasticsearch.plugin;
 
-import java.util.Collection;
-import java.util.Collections;
-
-import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.index.analysis.AnalysisModule;
 import org.elasticsearch.index.analysis.HangulChosungFilterFactory;
 import org.elasticsearch.index.analysis.HangulJamoTokenFilterFactory;
-import org.elasticsearch.indices.analysis.HangulJamoiIndicesAnalysisModule;
 import org.elasticsearch.plugins.Plugin;
 
 /**
@@ -25,14 +20,9 @@ public class HangulJamoPlugin extends Plugin {
 		return "Elasticsearch Analysis Hangul Jamo";
 	}
 	
-	@Override
-    public Collection<Module> nodeModules() {
-        return Collections.<Module>singletonList(new HangulJamoiIndicesAnalysisModule());
-    }
-	
 	public void onModule(AnalysisModule module) {
 		module.addTokenFilter("hangul_jamo", HangulJamoTokenFilterFactory.class);
 		module.addTokenFilter("hangul_chosung", HangulChosungFilterFactory.class);
 	}
-
+	
 }
